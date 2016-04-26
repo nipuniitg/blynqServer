120faa1d5a516e5a55301a005e4e96e70b3ec9f3
Nipun <blynqooha@gmail.com>
2016-04-26 19:25:37 +0530

    upsert playlist in one function

--- blynq/playlistManagement/playlist_urls.py
+++ blynq/playlistManagement/playlist_urls.py
@@ -5,7 +5,7 @@ from playlistManagement import views
 urlpatterns = [
     url(r'^$', views.index, name='playlist_index'),
     url(r'^getPlaylistsJson', views.get_playlist_items, name='playlists_json'),
-    url(r'^upsertPlaylistItems', views.upsert_playlist_items, name='upsert_playlist_items'),
+    # url(r'^upsertPlaylistItems', views.upsert_playlist_items, name='upsert_playlist_items'),
     url(r'^upsertPlaylist', views.upsert_playlist, name='upsert_playlist'),
     url(r'^deletePlaylist', views.delete_playlist, name='delete_playlist')
 ]
