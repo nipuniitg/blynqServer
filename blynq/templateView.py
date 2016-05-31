46fa2a4e7b07789e7a1bade213c3dd55707a60a5
Nipun Digital Ocean <blynqooha@bitbucket.org>
2016-05-31 07:37:44 -0400

    Fix template folder error in templateView.py

--- blynq/templateView.py
+++ blynq/templateView.py
@@ -4,13 +4,14 @@ from django.http import JsonResponse, HttpResponse
 
 @login_required
 def get_shared_partail_templates(request, template_name):
-    path = 'Shared/'
+    path = 'shared/'
     extn = '.html'
     return render(request, (path+template_name+extn))
 
 @login_required
 def get_content_partial_templates(request,template_name):
-    path = 'ContentManagement/'
+    print 'inside get_content_partial_templates'
+    path = 'contentManagement/'
     extn = '.html'
     return render(request, (path+template_name+extn))
 
