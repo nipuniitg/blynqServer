1ce4b628399fe5af49e9eabc55264617abf14086
Manikanta Kuriseti <kmpk123@gmail.com>
2016-06-09 10:07:38 +0530

    Introduced angular -ui-router and declared states

--- blynq/static/CustomScripts/app.js
+++ blynq/static/CustomScripts/app.js
@@ -1,6 +1,6 @@
 (function(){
     'use strict';
-    var mainApp =  angular.module('mainApp', ['sdApp', 'plApp','sagApp','hApp']).config(function($interpolateProvider) {
+    var mainApp =  angular.module('mainApp', ['ui.router','sdApp', 'plApp','sagApp','hApp']).config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('{[');
     $interpolateProvider.endSymbol(']}');
     });
@@ -10,5 +10,112 @@ mainApp.config(function($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 });
+
+mainApp.config(function($locationProvider,$stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("home");
+
+  //removes the hash for all the html5 supporting browsers. However for other, it comes
+  $locationProvider.html5Mode(true).hashPrefix('!')
+  //
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/home"
+      ,templateUrl: "/static/templates/home.html"
+      ,controller : 'homeCtrl'
+      ,controllerAs : 'homeCtrl'
+    })
+    .state('screens', {
+      url: "/screen"
+      ,templateUrl: "/static/templates/screen/screens.html"
+      ,controller: 'screenCtrl'
+      ,controllerAs : 'screenCtrl'
+    })
+    .state('groups', {
+      url: "/group"
+      ,templateUrl: "/static/templates/screen/groups.html"
+      ,controller : 'groupCtrl'
+      ,controllerAs : 'groupCtrl'
+    })
+    .state('playlists', {
+      url: "/playlist"
+      ,views : {
+            '' : {
+                   templateUrl: "/static/templates/playlistManagement/playlist_index.html"
+                  ,controller: 'plCtrl'
+                  ,controllerAs : 'plCtrl'
+            },
+            'content@playlists': {
+                    templateUrl : '/static/templates/contentManagement/content_holder.html'
+                    ,controller: 'ctCtrl'
+            }
+      }
+    })
+    .state('contentLibrary',{
+        url : '/contentLibrary'
+        ,views : {
+            '' : { templateUrl: "/static/templates/contentManagement/content_index.html" }
+            ,'content@contentLibrary' :{
+                templateUrl : '/static/templates/contentManagement/content_holder.html'
+                ,controller: 'ctCtrl'
+            }
+        }
+    })
+    .state('schedules',{
+        url: "/schedule"
+        ,templateUrl:'/static/templates/scheduleManagement/schedule_index.html'
+        ,controller : 'scheduleIndexCtrl'
+        ,controllerAs : 'scheduleIndexCtrl'
+    })
+});
+
+mainApp.factory("PrintToConsole", ["$rootScope", function ($rootScope) {
+    var handler = { active: false };
+    handler.toggle = function () { handler.active = !handler.active; };
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+        if (handler.active) {
+            console.log("$stateChangeStart --- event, toState, toParams, fromState, fromParams");
+            console.log(arguments);
+        };
+    });
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+        if (handler.active) {
+            console.log("$stateChangeError --- event, toState, toParams, fromState, fromParams, error");
+            console.log(arguments);
+        };
+    });
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        if (handler.active) {
+            console.log("$stateChangeSuccess --- event, toState, toParams, fromState, fromParams");
+            console.log(arguments);
+        };
+    });
+    $rootScope.$on('$viewContentLoading', function (event, viewConfig) {
+        if (handler.active) {
+            console.log("$viewContentLoading --- event, viewConfig");
+            console.log(arguments);
+        };
+    });
+    $rootScope.$on('$viewContentLoaded', function (event) {
+        if (handler.active) {
+            console.log("$viewContentLoaded --- event");
+            console.log(arguments);
+        };
+    });
+    $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
+        if (handler.active) {
+            console.log("$stateNotFound --- event, unfoundState, fromState, fromParams");
+            console.log(arguments);
+        };
+    });
+    return handler;
+}]);
+
+mainApp.run(['PrintToConsole', function(PrintToConsole) {
+    PrintToConsole.active = false;
+}]);
+
 
 }());
