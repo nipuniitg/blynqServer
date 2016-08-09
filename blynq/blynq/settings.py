d94febc19d350bd4f1ab5f3e937cd75a2e70d540
Nipun <blynqooha@gmail.com>
2016-08-09 12:17:48 +0530

    Debug should be false by default

--- blynq/blynq/settings.py
+++ blynq/blynq/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1ig8fo2929x)i=c7k$z0qe#@1n())0o2rt7*45^j^td5_duj$m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = []
