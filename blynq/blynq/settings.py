250c218127b59eee32a2520e99f2afe7488cd202
Nipun <blynqooha@gmail.com>
2016-08-09 19:07:44 +0530

    Added django-fcm library

--- blynq/blynq/settings.py
+++ blynq/blynq/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
     'schedule', # https://github.com/llazzaro/django-scheduler
     'reversion',    # http://django-reversion.readthedocs.io/en/latest/
     'coverage',
+    'fcm',
     'authentication',
     'screenManagement',
     'contentManagement',
