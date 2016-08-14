36eb2958cd9a4dc6366b8d379d6c665b587b0aac
Manikanta Kuriseti <kmpk123@gmail.com>
2016-08-14 13:23:32 +0530

    bug-resolved : selected widgets not appearing in playlists-text-box

--- blynq/static/CustomScripts/shared/blueprints.js
+++ blynq/static/CustomScripts/shared/blueprints.js
@@ -32,6 +32,7 @@
             this.schedule_pane_id = -1;
             this.layout_pane = angular.copy(layout_pane);
             this.schedule_playlists = [];
+            this.schedule_widgets=[];
             this.timeline ={
                 is_always   : !0
                 ,start_date  : null
