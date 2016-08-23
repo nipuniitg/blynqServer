fc3d3550ad28c94d181cb6bdd01a390d06d78409
Nipun <blynqooha@gmail.com>
2016-08-23 12:02:40 +0530

    Screen report data

--- blynq/reports/views.py
+++ blynq/reports/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 from customLibrary.views_lib import get_userdetails, generate_utc_datetime, get_ist_date_str, obj_to_json_response, \
     debugFileLog, string_to_dict
-from reports.models import ScreenAnalytics
+from playlistManagement.models import Playlist
+from reports.models import ScreenAnalytics, MediaAnalytics
 from screenManagement.models import Screen
 
 
@@ -50,8 +51,77 @@ def intersection_time(event1_start, event1_end, event2_start, event2_end):
     return delta
 
 
+def screen_filter(filter_set, user_details):
+    all_screens = filter_set.get('all_screens')
+    if all_screens:
+        screen_ids = Screen.get_user_relevant_objects(user_details=user_details).values_list('screen_id', flat=True)
+    else:
+        screen_objects = filter_set.get('screens')
+        screen_ids = (item['screen_id'] for item in screen_objects)
+    return screen_ids
+
+
+def playlist_filter(filter_set, user_details):
+    all_playlists = filter_set.get('all_playlists')
+    if all_playlists:
+        playlist_ids = Playlist.get_user_relevant_objects(user_details=user_details).values_list('playlist_id', flat=True)
+    else:
+        playlist_objects = filter_set.get('playlists')
+        playlist_ids = (item['playlist_id'] for item in playlist_objects)
+    return playlist_ids
+
+
 def screen_reports(request):
-    screen_report_list = []
+    # Input filter_set
+    # start_date, end_date, start_time, end_time : ist datetime
+    # all_screens : bool, screens : array of screen objects
+    # output : For each screen: screen object, time_active (seconds),
+    #       total_time (end_date - start_date + 1)*(end_time - start_time)
+    screen_report_list, date_str_list, time_active_list, pie_chart_data, table_data = ([] for i in range(5))
+    line_chart_data = {'date_str': [], 'time_active': []}
+    json_dict = {'line_chart_data': line_chart_data, 'pie_chart_data': pie_chart_data, 'table_data': table_data }
+    try:
+        user_details = get_userdetails(request)
+        posted_data = string_to_dict(request.body)
+        filter_set = posted_data.get('filterset')
+        screen_ids = screen_filter(filter_set, user_details)
+        start_datetime, end_date, time_difference = date_time_filters(filter_set=filter_set)
+        total_days = (end_date - start_datetime.date()).days
+        total_time_requested = total_days * time_difference.seconds
+        if end_date < start_datetime.date():
+            return obj_to_json_response(json_dict)
+        all_screens_dict = {}
+        total_time_active = 0
+        for single_date in date_range(start_date=start_datetime.date(), end_date=end_date):
+            start = datetime.combine(single_date, start_datetime.time())
+            end = start + time_difference
+            date_str = get_ist_date_str(start)
+            date_str_list.append(date_str)
+            screen_analytics = ScreenAnalytics.objects.exclude(
+                screen_id__in=screen_ids, session_start_time__gte=end, session_end_time__lte=start_datetime).order_by('screen_id')
+            date_active_time = 0    # active_time of all_screens in a single date
+            for obj in screen_analytics:
+                time_active = intersection_time(start_datetime, end, obj.session_start_time, obj.session_end_time)
+                date_active_time += time_active
+                total_time_active += time_active
+                screen_id_key = str(obj.screen_id)
+                if all_screens_dict.get(screen_id_key) and all_screens_dict[screen_id_key]['time_active']:
+                    all_screens_dict[str(obj.screen_id)]['time_active'] += time_active
+                else:
+                    all_screens_dict[str(obj.screen_id)] = {
+                        'screen_id': obj.screen_id, 'screen_name': obj.screen_name,
+                        'time_active': time_active, 'total_time_requested': total_time_requested}
+            time_active_list.append(date_active_time)
+        if total_time_requested >= total_time_active:
+            pie_chart_data = [total_time_active, total_time_requested-total_time_active]
+        table_data = all_screens_dict.values()
+        json_dict = {'line_chart_data': line_chart_data, 'pie_chart_data': pie_chart_data, 'table_data': table_data }
+    except Exception as e:
+        debugFileLog.exception(e)
+    return obj_to_json_response(json_dict)
+
+
+def playlist_reports(request):
     try:
         user_details = get_userdetails(request)
         posted_data = string_to_dict(request.body)
@@ -59,51 +129,11 @@ def screen_reports(request):
         # Input filter_set
         # start_date, end_date, start_time, end_time : ist datetime
         # all_screens : bool, screens : array of screen objects
-        # output : For each screen: screen object, time_active (seconds),
-        #       total_time (end_date - start_date + 1)*(end_time - start_time)
-        all_screens = filter_set.get('all_screens')
-        if all_screens:
-            screen_ids = Screen.get_user_relevant_objects(user_details=user_details).values_list('screen_id', flat=True)
-        else:
-            screen_objects = filter_set.get('screens')
-            screen_ids = (item['screen_id'] for item in screen_objects)
-        start_datetime, end_date, time_difference = date_time_filters(filter_set=filter_set)
-        total_days = (end_date - start_datetime.date()).days
-        total_time_requested = total_days * time_difference.seconds
-        if end_date < start_datetime.date():
-            return obj_to_json_response(screen_report_list)
-        for single_date in date_range(start_date=start_datetime.date(), end_date=end_date):
-            start = datetime.combine(single_date, start_datetime.time())
-            end = start + time_difference
-            date_str = get_ist_date_str(start)
-            screen_analytics = ScreenAnalytics.objects.exclude(
-                screen_id__in=screen_ids, session_start_time__gte=end, session_end_time__lte=start_datetime).order_by('screen_id')
-            all_screens_dict = {}
-            for obj in screen_analytics:
-                time_active = intersection_time(start_datetime, end, obj.session_start_time, obj.session_end_time)
-                screen_id_key = str(obj.screen_id)
-                if all_screens_dict.get(screen_id_key) and all_screens_dict[screen_id_key]['time_active']:
-                    all_screens_dict[str(obj.screen_id)]['time_active'] += time_active
-                else:
-                    all_screens_dict[str(obj.screen_id)] = {
-                        'date_str': date_str, 'screen_id': obj.screen_id, 'screen_name': obj.screen_name,
-                        'time_active': time_active, 'total_time_requested': total_time_requested}
-            screen_report_list.extend(list(all_screens_dict.values()))
-    except Exception as e:
-        debugFileLog.exception(e)
-    return obj_to_json_response(screen_report_list)
-
-
-def playlist_reports(request):
-    try:
-        user_details = get_userdetails(request)
-        params = request.GET
-        filter_set = params.get('filterset')
-        # Input filter_set
-        # start_date, end_date, start_time, end_time : ist datetime
-        # all_screens : bool, screens : array of screen objects
         # all_playlists : bool, playlists: array of playlist objects
         # output :
+        screen_ids = screen_filter(filter_set=filter_set, user_details=user_details)
+        playlist_ids = playlist_filter(filter_set=filter_set, user_details=user_details)
+        media_analytics = MediaAnalytics.objects.filter(screen_id__in=screen_ids)
     except Exception as e:
         debugFileLog.exception(e)
 
