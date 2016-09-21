f90587b4ef30647062cbc0ec28155a0697ec56c1
Nipun Edara <blynqooha@bitbucket.org>
2016-09-21 15:30:51 -0400

    migrations for merging v1-widgets-rss to blynq-dev-v1 branch

---
+++ blynq/scheduleManagement/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('scheduleManagement', '0017_auto_20160811_1309'),
+        ('scheduleManagement', '0017_auto_20160827_1229'),
+    ]
+
+    operations = [
+    ]
