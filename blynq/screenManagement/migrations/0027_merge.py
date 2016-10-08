cccbb7e889d7d47566075668d89e4ef6894f94af
Nipun <blynqooha@gmail.com>
2016-10-08 17:51:43 +0530

    Fixed the migrations issue in blynq-dev-v1 :D

---
+++ blynq/screenManagement/migrations/0027_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('screenManagement', '0023_auto_20160816_2041'),
+        ('screenManagement', '0026_auto_20160928_1502'),
+    ]
+
+    operations = [
+    ]
