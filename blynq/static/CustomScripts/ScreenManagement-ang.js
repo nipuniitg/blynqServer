3311420459a114dc8839ef44380837ed216d2471
Manikanta Kuriseti <kmpk123@gmail.com>
2016-12-05 23:22:15 +0530

    spaces added in time since

--- blynq/static/CustomScripts/ScreenManagement-ang.js
+++ blynq/static/CustomScripts/ScreenManagement-ang.js
@@ -755,20 +755,20 @@ sagApp.filter('timeSince', function(){
         var seconds = Math.floor(((new Date().getTime()/1000) - date)),
         interval = Math.floor(seconds / 31536000);
 
-        if (interval > 1) return interval + "yrs ago";
+        if (interval > 1) return interval + " yrs ago";
 
         interval = Math.floor(seconds / 2592000);
-        if (interval > 1) return interval + "mons ago";
+        if (interval > 1) return interval + " mons ago";
 
         interval = Math.floor(seconds / 86400);
-        if (interval >= 1) return interval + "days ago";
+        if (interval >= 1) return interval + " days ago";
 
         interval = Math.floor(seconds / 3600);
-        if (interval >= 1) return interval + "hrs ago";
+        if (interval >= 1) return interval + " hrs ago";
 
         interval = Math.floor(seconds / 60);
-        if (interval > 1) return interval + "mins ago";
+        if (interval > 1) return interval + " mins ago";
 
-        return Math.floor(seconds) + "secs ago";
+        return Math.floor(seconds) + " secs ago";
     }
 })
